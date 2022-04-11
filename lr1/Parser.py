de13c1a139a1bde57a227bd322470498f9635332
Vadim <9536611@gmail.com>
2022-04-11 22:14:31 +0400

    lr4 complete

--- lr1/Parser.py
+++ lr1/Parser.py
@@ -7,6 +7,7 @@ class Parser:
         f = open(path, "r")
         list = f.read().split("\n")
         vertexList = []
+        vertexListVN = []
         for row in list:
             if row.startswith("v "):
                 vertexList.append(row)
@@ -14,7 +15,28 @@ class Parser:
         for vertex in vertexList:
             coordinates = vertex.split(" ")
             coordList.append(Coordinate(float(coordinates[1]), float(coordinates[2]), float(coordinates[3])))
-        return coordList
+        for row in list:
+            if row.startswith("vn "):
+                vertexListVN.append(row)
+        coordListVN = []
+        for vertex in vertexListVN:
+            coordinates = vertex.split(" ")
+            coordListVN.append(Coordinate(float(coordinates[1]), float(coordinates[2]), float(coordinates[3])))
+        return coordList, coordListVN
+
+    # @staticmethod
+    # def getNormals(path):
+    #     f = open(path, "r")
+    #     list = f.read().split("\n")
+    #     vertexListVN = []
+    #     for row in list:
+    #         if row.startswith("vn "):
+    #             vertexListVN.append(row)
+    #     coordListVN = []
+    #     for vertex in vertexList:
+    #         coordinates = vertex.split(" ")
+    #         coordListVN.append(Coordinate(float(coordinates[1]), float(coordinates[2]), float(coordinates[3])))
+    #     return coordListVN
 
     @staticmethod
     def getPolygons(path):
@@ -25,10 +47,15 @@ class Parser:
             if row.startswith("f "):
                 polygonList.append(row)
         resultList = []
+        normalList = []
         for polRow in polygonList:
             vertexIndexes = polRow.split(" ")
             vertexIndex1 = vertexIndexes[1].split("/")[0]
             vertexIndex2 = vertexIndexes[2].split("/")[0]
             vertexIndex3 = vertexIndexes[3].split("/")[0]
+            normalIndex1 = vertexIndexes[1].split("/")[2]
+            normalIndex2 = vertexIndexes[2].split("/")[2]
+            normalIndex3 = vertexIndexes[3].split("/")[2]
             resultList.append([int(vertexIndex1), int(vertexIndex2), int(vertexIndex3)])
-        return resultList
+            normalList.append([int(normalIndex1), int(normalIndex2), int(normalIndex3)])
+        return resultList, normalList
--- main.py
+++ main.py
@@ -27,12 +27,14 @@ def projectiveTransformation(x, y, z, ax, ay, u0, v0, tz):
 class OBJ3DModel:
     def __init__(self):
         self.vertexList: Optional[np.ndarray] = None
+        self.vertexListVN: Optional[np.ndarray] = None
         self.polyVertexIndexesList: Optional[np.ndarray] = None
+        self.polyNormalIndexesList: Optional[np.ndarray] = None
         self.img = MyImage()
 
     def read_model(self, path: str):
-        self.vertexList = Parser.getVertexes(path)
-        self.polyVertexIndexesList = Parser.getPolygons(path)
+        self.vertexList, self.vertexListVN = Parser.getVertexes(path)
+        self.polyVertexIndexesList, self.polyNormalIndexesList = Parser.getPolygons(path)
 
     def arr_init(self):
         self.vertexList = np.zeros((self.height, self.width, self.channels), dtype=np.uint8)
@@ -55,6 +57,35 @@ class OBJ3DModel:
             self.vertexList[i].x = coordinatesFinish[i, 0]
             self.vertexList[i].y = coordinatesFinish[i, 1]
             self.vertexList[i].z = coordinatesFinish[i, 2]
+
+    def rotateWithNormal(self, alpha, beta, gamma):
+        alpha *= np.pi / 180
+        beta *= np.pi / 180
+        gamma *= np.pi / 180
+        R = np.mat([[1.0, 0, 0], [0, np.cos(alpha), np.sin(alpha)], [0, -np.sin(alpha), np.cos(alpha)]]) \
+            * np.mat([[np.cos(beta), 0, np.sin(beta)], [0, 1.0, 0], [-np.sin(beta), 0, np.cos(beta)]]) \
+            * np.mat([[np.cos(gamma), np.sin(gamma), 0], [-np.sin(gamma), np.cos(gamma), 0], [0, 0, 1.0]])
+
+        coordinatesFinish = np.zeros((len(self.vertexList), 3), np.float64)
+        for i in range(len(self.vertexList)):
+            coordinatesFinish[i, 0] = self.vertexList[i].x
+            coordinatesFinish[i, 1] = self.vertexList[i].y
+            coordinatesFinish[i, 2] = self.vertexList[i].z
+        coordinatesFinish = coordinatesFinish.dot(R).getA()
+        for i in range(len(self.vertexList)):
+            self.vertexList[i].x = coordinatesFinish[i, 0]
+            self.vertexList[i].y = coordinatesFinish[i, 1]
+            self.vertexList[i].z = coordinatesFinish[i, 2]
+        coordinatesFinish2 = np.zeros((len(self.vertexList), 3), np.float64)
+        for i in range(len(self.vertexListVN)):
+            coordinatesFinish2[i, 0] = self.vertexListVN[i].x
+            coordinatesFinish2[i, 1] = self.vertexListVN[i].y
+            coordinatesFinish2[i, 2] = self.vertexListVN[i].z
+        coordinatesFinish2 = coordinatesFinish2.dot(R).getA()
+        for i in range(len(self.vertexListVN)):
+            self.vertexListVN[i].x = coordinatesFinish2[i, 0]
+            self.vertexListVN[i].y = coordinatesFinish2[i, 1]
+            self.vertexListVN[i].z = coordinatesFinish2[i, 2]
 
     def perspective(self, ax, ay, u0, v0, zsup):
         for i in range(len(self.vertexList)):
@@ -165,7 +196,7 @@ class OBJ3DModel:
         self.read_model(path)
         self.centrilize()
         self.normalize()
-        self.rotate(0, -45, 0)
+        self.rotate(0, 90, 0)
         self.perspective(2000, 2000, 0, 0, 3)
         self.normalize()
         for vertexIndexes in self.polyVertexIndexesList:
@@ -191,6 +222,38 @@ class OBJ3DModel:
                                          scaleX * self.vertexList[vertexIndexes[2] - 1].z,
                                          self.img.zBuffer, (255 * cosine, 0, 0))
 
+    def draw_guro_edges(self, path: str):
+        self.read_model(path)
+        self.centrilize()
+        self.normalize()
+        self.rotateWithNormal(0, 45, 0)
+        self.perspective(-2000, -2000, -800, -800, 10)
+        for i in range(len(self.polyVertexIndexesList)):
+            vertexIndexes = self.polyVertexIndexesList[i]
+            normalIndexes = self.polyNormalIndexesList[i]
+            nx1 = self.vertexListVN[normalIndexes[0] - 1].x
+            ny1 = self.vertexListVN[normalIndexes[0] - 1].y
+            nz1 = self.vertexListVN[normalIndexes[0] - 1].z
+            nx2 = self.vertexListVN[normalIndexes[1] - 1].x
+            ny2 = self.vertexListVN[normalIndexes[1] - 1].y
+            nz2 = self.vertexListVN[normalIndexes[1] - 1].z
+            nx3 = self.vertexListVN[normalIndexes[2] - 1].x
+            ny3 = self.vertexListVN[normalIndexes[2] - 1].y
+            nz3 = self.vertexListVN[normalIndexes[2] - 1].z
+            cosine1 = scalarProduct(nx1, ny1, nz1, 0, 0, 1)
+            cosine2 = scalarProduct(nx2, ny2, nz2, 0, 0, 1)
+            cosine3 = scalarProduct(nx3, ny3, nz3, 0, 0, 1)
+
+            self.img.drawTriangle_v3(self.vertexList[vertexIndexes[0] - 1].x,
+                                     self.vertexList[vertexIndexes[0] - 1].y,
+                                     self.vertexList[vertexIndexes[0] - 1].z,
+                                     self.vertexList[vertexIndexes[1] - 1].x,
+                                     self.vertexList[vertexIndexes[1] - 1].y,
+                                     self.vertexList[vertexIndexes[1] - 1].z,
+                                     self.vertexList[vertexIndexes[2] - 1].x,
+                                     self.vertexList[vertexIndexes[2] - 1].y,
+                                     self.vertexList[vertexIndexes[2] - 1].z,
+                                     self.img.zBuffer, cosine1, cosine2, cosine3)
 
 
 class MyImage:
@@ -341,17 +404,43 @@ class MyImage:
                         zBuffer[xIndex, yIndex] = sourceZ
         pass
 
+    def drawTriangle_v3(self, x0, y0, z0, x1, y1, z1, x2, y2, z2, zBuffer,
+                        l1, l2, l3):
+        xmin = min(x0, x1, x2) if min(x0, x1, x2) < 0 else 0
+        ymin = min(y0, y1, y2) if min(y0, y1, y2) < 0 else 0
+        xmax = max(x0, x1, x2) if max(x0, x1, x2) < self.width else self.width
+        ymax = max(y0, y1, y2) if max(y0, y1, y2) < self.height else self.height
+        if xmax < (-self.width):
+            xmax = -self.width
+        if ymax < (-self.height):
+            ymax = -self.height
+        if xmin < (-self.width):
+            xmin = -self.width
+        if ymin < (-self.height):
+            ymin = -self.height
+        for xIndex in range(int(xmin), int(xmax)):
+            for yIndex in range(int(ymin), int(ymax)):
+                if xIndex > 0:
+                    break
+                if yIndex > 0:
+                    break
+
+                bar1, bar2, bar3 = convertToBarycentric(xIndex, yIndex, x0, y0, x1, y1, x2, y2)
+                colorSub = l1*bar1 + l2*bar2 + l3*bar3
+                if colorSub <= 0:
+                    if bar1 >= 0 and bar2 >= 0 and bar3 >= 0:
+                        sourceZ = bar1 * z0 + bar2 * z1 + bar3 * z2
+                        if sourceZ < zBuffer[xIndex, yIndex]:
+                            self.set_pixel(xIndex, yIndex, (int(-colorSub * 255), 0, 0))
+                            zBuffer[xIndex, yIndex] = sourceZ
+        pass
+
 
 def convertToBarycentric(x, y, x0: float, y0: float, x1: float, y1: float, x2: float, y2: float, eps=0.1):
     lambda0 = ((x1 - x2) * (y - y2) - (y1 - y2) * (x - x2)) / ((x1 - x2) * (y0 - y2) - (y1 - y2) * (x0 - x2))
     lambda1 = ((x2 - x0) * (y - y0) - (y2 - y0) * (x - x0)) / ((x2 - x0) * (y1 - y0) - (y2 - y0) * (x1 - x0))
     lambda2 = ((x0 - x1) * (y - y1) - (y0 - y1) * (x - x1)) / ((x0 - x1) * (y2 - y1) - (y0 - y1) * (x2 - x1))
-    if abs(lambda0 + lambda1 + lambda2 - 1.0) < eps:
-        return lambda0, lambda1, lambda2
-    else:
-        print(str(lambda0) + ' + ' + str(lambda1) + ' + ' + str(lambda2) + ' = ' + str(lambda2 + lambda1 + lambda0))
-        print("Сумма барицентрических координат не равняется 1")
-        return 0, 0, 0
+    return lambda0, lambda1, lambda2
 
 
 def doLR1():
@@ -411,22 +500,31 @@ def doLR2():
     obj.img.save("lr2/deer_lighted_colored.jpg")
     obj.img.imshow()
 
+
 def doLR3():
     # drawTriangle(300, 500, 20, 30, 150, 700)
     obj = OBJ3DModel()
     obj.img.width = 1600
     obj.img.height = 1600
     obj.img.arr_init()
-    # obj.draw_edges_v1("lr1/deer.obj", 650, 0, 1, -1)
-    # obj.draw_edges_v2("lr1/deer.obj", 650, 0, 1, -1)
-    # obj.img.save("lr2/deer_colored.jpg")
     obj.img.zBuffer_init()
     obj.draw_projective_edges("lr1/deer.obj", 800, 800, 400, -400)
     obj.img.save("lr3/deer_lighted_colored_foved.jpg")
     obj.img.imshow()
 
 
+def doLR4():
+    obj = OBJ3DModel()
+    obj.img.width = 1600
+    obj.img.height = 1600
+    obj.img.arr_init()
+    obj.img.zBuffer_init()
+    obj.draw_guro_edges("lr4/rabbit.obj")
+    obj.img.save("lr4/rabbit_guro.jpg")
+    obj.img.imshow()
+
+
 if __name__ == "__main__":
-    doLR3()
+    doLR4()
     print('done')
     pass
